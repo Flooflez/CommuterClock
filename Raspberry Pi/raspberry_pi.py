eb11f9a68e6a54ceedc584174479d9acede4db5c
Flooflez <maxmyta@gmail.com>
2023-03-21 19:29:22 -0400

    Added scheduling system
    Also changed testing user_id.txt just for testing and debugging

--- Raspberry Pi/raspberry_pi.py
+++ Raspberry Pi/raspberry_pi.py
@@ -1,8 +1,12 @@
+import time
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import googlemaps
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import sched
 
 #
 # Constants
@@ -36,6 +40,17 @@ uptown_downtown_dictionary = {
     "Jamaica-179 St": 1,
 }
 
+#
+# Global Vars
+#
+
+global wait_seconds, destination, origin, start_hour, end_hour, should_consider_car, user_id
+global start_time, end_time  # parsed from strings
+gmaps = googlemaps.Client(key='AIzaSyCnGQ5oITGwl8B9TJckesr5X__rCnJ3klI')
+cred = credentials.Certificate("Private Key/commuter-clock-firebase-adminsdk-q5p6m-d316723468.json")
+firebase_admin.initialize_app(cred)
+scheduler = sched.scheduler(time.time, time.sleep)
+
 
 #
 # Functions
@@ -43,6 +58,7 @@ uptown_downtown_dictionary = {
 
 def get_user_id():
     reader = open("user_id.txt", "r")
+    global user_id
     user_id = reader.read();
     reader.close()
     return user_id
@@ -55,7 +71,7 @@ def check_uptown_downtown(headsign):
     return -1
 
 
-def get_line_info(origin, destination, gmaps):
+def get_line_info():
     directions_result = gmaps.directions(origin, destination, mode="transit", departure_time=datetime.now(),
                                          alternatives=False)
     for leg in directions_result[0]['legs']:
@@ -71,32 +87,37 @@ def get_line_info(origin, destination, gmaps):
                     return line['short_name'], direction, minutes
 
 
-def get_car_info(origin, destination, gmaps):
+def get_car_info():
     directions_result = gmaps.directions(origin, destination, mode="driving", departure_time=datetime.now(),
                                          alternatives=False)
     duration_time_seconds = directions_result[0]['legs'][0]['duration']['value'];
     traffic_duration_time_seconds = directions_result[0]['legs'][0]['duration_in_traffic']['value'];
-    minutes = round(max(duration_time_seconds,traffic_duration_time_seconds)/60)
+    minutes = round(max(duration_time_seconds, traffic_duration_time_seconds) / 60)
 
-    return minutes;
+    return minutes
 
 
-def update_display(origin, destination, gmaps, should_consider_car):
-    line_num, direction, minutes = get_line_info(origin, destination, gmaps)
+def update_display():
+    #DEBUG ONLY
+    print("Spin the thing")
+    return #DEBUG ONLY
+
+    line_num, direction, minutes = get_line_info()
     if should_consider_car:
-        car_minutes = get_car_info(origin, destination, gmaps)
+        car_minutes = get_car_info()
         if car_minutes < minutes:
             print("Car, Minutes:", minutes)
     else:
         print(line_num, "Train, Direction:", direction, ", Minutes:", minutes)
 
 
-def get_document_data(user_id):
+def get_document_data():
     db = firestore.client()
     doc_ref = db.collection(u'settings').document(user_id)
     doc = doc_ref.get()
 
     if doc.exists:
+        global wait_seconds, destination, origin, start_hour, end_hour, should_consider_car, start_time, end_time
         doc_dictionary = doc.to_dict()
         wait_seconds = doc_dictionary['wait_seconds']
         destination = doc_dictionary['destination']
@@ -105,34 +126,66 @@ def get_document_data(user_id):
         end_hour = doc_dictionary['end_hour']
         should_consider_car = doc_dictionary['should_consider_car']
 
-        return wait_seconds, destination, origin, start_hour, end_hour, should_consider_car
+        start_time = datetime.strptime(start_hour, '%H:%M').time()
+        end_time = datetime.strptime(end_hour, '%H:%M').time()
     else:
         raise Exception("User ID does not exist")
+
+
+def perform_update():
+    update_display()
+    if datetime.now().time() < end_time:
+        scheduler.enter(5, 1, perform_update, ())
+        print("DEBUG: another update! " + str(datetime.now().time()))
+    else:
+        # done for today, set next event for tomorrow
+        scheduled_time = (datetime.now().replace(hour=start_time.hour, minute=start_time.minute, second=0) + timedelta(
+            1)).timestamp()
+        scheduler.enterabs(scheduled_time, 1, perform_update, ())
+
+
+
+def sync_button_pressed():
+    # clear scheduler
+    list(map(scheduler.cancel, scheduler.queue))
+    get_document_data()
+
+    schedule_start()
+
+
+def schedule_start():
+    if datetime.now().time() > end_time:
+        # schedule for tomorrow
+        scheduled_time = (datetime.now().replace(hour=start_time.hour, minute=start_time.minute, second=0) + timedelta(
+            1)).timestamp()
+        scheduler.enterabs(scheduled_time, 1, perform_update, ())
+    elif datetime.now().time() > start_time:
+        # start now
+        perform_update()
+    else:
+        # schedule for today
+        scheduled_time = datetime.now().replace(hour=start_time.hour, minute=start_time.minute, second=0).timestamp()
+        scheduler.enterabs(scheduled_time, 1, perform_update, ())
+
+    scheduler.run()
 
 
 #
 # Init API
 #
 def main():
-    cred = credentials.Certificate("Private Key/commuter-clock-firebase-adminsdk-q5p6m-d316723468.json")
-    firebase_admin.initialize_app(cred)
-
-    gmaps = googlemaps.Client(key='AIzaSyCnGQ5oITGwl8B9TJckesr5X__rCnJ3klI')
-
+    global user_id
     user_id = get_user_id()
-    print(user_id)
-    wait_seconds, destination, origin, start_hour, end_hour, should_consider_car = get_document_data(user_id)
+    get_document_data()
+    print(start_time, end_time)
 
-    # insert loop:
-    # check button press
-    # get_document_data
-    # update_display(origin, destination, gmaps, should_consider_car)
-    # DO NOT CALL update_display UNLESS NECESSARY.
-    # wait
+    # schedule the event
+    schedule_start()
+
+    print("done")
 
     # Tasks:
-    # check delay/sudden line/time change
-    # check if driving faster
+    # check delay/sudden line/time change?
 
 
 main()
